060dda9201236bb369785aa706d98fd3527db25f
Blazer5544 <javiermorales2075@gmail.com>
2024-12-05 11:58:29 -0500

    Completed Animations and started work on Dash

--- Final Project/Assets/Scripts/PlatformMovement.cs
+++ Final Project/Assets/Scripts/PlatformMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlatformMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private TrailRenderer _trailrenderer;
     [SerializeField]
     private Transform groundCheck;
     [SerializeField]
@@ -14,19 +16,39 @@ public class PlatformMovement : MonoBehaviour
     [SerializeField]
     private float jumpForce = 10f;
     [SerializeField]
+    private float _yVelJumpReleaseMod = 2f;
+    [SerializeField]
     private LayerMask collisionMask;
+
+    [SerializeField] private float _dashingVelocity = 14f;
+    [SerializeField] private float _dashingTime = 0.5f;
+    private Vector2 _dashingDir;
+    private bool _isDashing;
+    private bool _canDash;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _trailrenderer = GetComponent<_trailrenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         var inputX = Input.GetAxisRaw("Horizontal");
-        var jumpInput = Input.GetButton("Jump");
+        var jumpInput = Input.GetButtonDown("Jump");
+        var jumpInputReleased = Input.GetButtonUp("Jump");
+        var dashInput = Input.GetButtonDown("Dash");
+
+        if(dashInput && _canDash)
+        {
+            _isDashing = true;
+            _canDash = false;
+            _trailrenderer.emitting = true;
+            _dashingDir = new Vector2(inputX, Input.GetAxisRaw("Vertical"));
+        }
 
         _rigidbody.velocity = new Vector2(inputX * speed, _rigidbody.velocity.y);
 
@@ -35,13 +57,21 @@ public class PlatformMovement : MonoBehaviour
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
         }
 
+        if(jumpInputReleased && _rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y / _yVelJumpReleaseMod);
+        }
+
         if(inputX != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(inputX), 1, 1);
         }
+
+        _animator.SetBool("IsJumping", !IsGrounded());
+        _animator.SetBool("IsRunning", inputX != 0);
     }
 
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionMask);
     }
